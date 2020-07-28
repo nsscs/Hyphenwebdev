c1955ed14c36f8a00adaee0ae010b3c388a0da5f
MUKTA A A <mukthakutty@gmail.com>
2020-07-28 16:40:55 +0530

    Add files via upload

---
+++ hyphen.js
@@ -0,0 +1,12 @@
+function popup(){
+
+
+var message = document.getElementsById("comment").value;
+alert(message);
+}
+function changecolor(){
+    var color = document.getElementsById("color-control").style.color;
+        document.getElementsById("color-control").style.color = "#2CBEC4";
+   
+}
+}
